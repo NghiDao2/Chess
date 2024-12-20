fcab5c144ef8e5c805f5280ac70f835180ffc7f8
root <root@NghiLaptop>
2024-12-20 12:47:11 -0800

    fix simulation_batch

--- simulation_batch.py
+++ simulation_batch.py
@@ -9,8 +9,8 @@ def game_worker(sim):
         with sim.lock:
             while sim.queue.empty() and not sim.thread_exit:
                 sim.added.wait()
-            if thread_exit:
-                return 
+            if sim.thread_exit:
+                return
             game = sim.queue.get()
 
         game.run()
@@ -19,44 +19,42 @@ def game_worker(sim):
             sim.finished.notify_all()
 
 
-class SimulationBatch():
+class SimulationBatch:
 
     def __init__(self, num_threads=4):
-        
         self.thread_exit = False
         self.queue = queue.Queue()
         self.lock = threading.Lock()
-        self.added = threading.Condition(lock)
-        self.finished = threading.Condition(lock)
+        self.added = threading.Condition(self.lock)
+        self.finished = threading.Condition(self.lock)
 
-        
         self.threads = [
-            threading.Thread(target=game_worker, args=(self,)) for i in range(NUM_THREADS)
+            threading.Thread(target=game_worker, args=(self,)) for i in range(num_threads)
         ]
 
         for thread in self.threads:
             thread.start()
 
-
-    def add_game(white_bot, black_bot, move_time=300, move_limit=200):
+    def add_game(self, white_bot, black_bot, move_time=300, move_limit=200):
         with self.lock:
-            game = GameSimulator(white_bot, black_bot, move_time=move_time, move_limit=200)
+            game = GameSimulator(white_bot, black_bot, move_time=move_time, move_limit=move_limit)
             self.queue.put(game)
             self.added.notify()
             return game
 
-    def get_queue_size():
+    def get_queue_size(self):
         with self.lock:
             return self.queue.qsize()
 
-    def wait_finish():
-        with lock:
-            self.finished.wait()
+    def wait_finish(self):
+        with self.lock:
+            while not self.queue.empty():
+                self.finished.wait()
 
-    def exit():
-        with lock:
+    def exit(self):
+        with self.lock:
             self.thread_exit = True
             self.added.notify_all()
 
-            for thread in self.threads:
-                thread.join()
+        for thread in self.threads:
+            thread.join()
